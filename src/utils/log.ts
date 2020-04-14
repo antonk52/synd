517e36db903b5ec5d97d980812cc0b37efcf21e4
antonk52 <antonk52@yandex-team.ru>
2020-04-14 23:07:24 +0300

    fix timestamp

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -4,8 +4,11 @@ import kleur from 'kleur';
 
 const stamp = (): string => {
     const d = new Date();
+    const hours = d.getHours().toString().padStart(2, '0');
+    const minutes = d.getMinutes().toString().padStart(2, '0');
+    const seconds = d.getSeconds().toString().padStart(2, '0');
 
-    return `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`;
+    return `[${hours}:${minutes}:${seconds}]`;
 };
 
 const log = (text: string): void => {
